2fedf9c99c8dc81b1711d7682044428a22152582
Naoufel <nberrada@lithium.paris>
2025-05-04 12:32:32 +0200

    add: analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Analytics } from "@vercel/analytics/react"
 import MercatorMap from "@/components/mercator-map"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
+      <Analytics />
       <MercatorMap />
     </main>
   )
