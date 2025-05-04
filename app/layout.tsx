83f63fd89b4062a97a55cc1d83930cb7ddcca688
Naoufel <nberrada@lithium.paris>
2025-05-04 11:30:05 +0200

    add: icon and stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,14 @@ export const metadata: Metadata = {
   title: 'Mercator map projector',
   description: 'View the classic mercator projection from an other angle',
   generator: 'v0.dev',
+  icons: {
+    icon: '/icon.png',
+    shortcut: '/icon.png',
+    apple: '/icon.png',
+  },
+  openGraph: {
+    images: '/cover.png',
+  },
 }
 
 export default function RootLayout({
